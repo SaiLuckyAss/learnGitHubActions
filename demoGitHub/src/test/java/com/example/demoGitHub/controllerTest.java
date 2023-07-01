51cdf09479153585f02d9cfbd231bd132330b6a8
saiKumarKulai <118827147+saiKumarKulai@users.noreply.github.com>
2023-07-01 21:39:57 +0530

    Create controllerTest.java

---
+++ demoGitHub/src/test/java/com/example/demoGitHub/controllerTest.java
@@ -0,0 +1,29 @@
+package com.example.demoGitHub;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.*;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(controller.class)
+class controllerTest {
+    @Autowired
+    private MockMvc mvc;
+    @Test
+    public void ctest() throws Exception {
+        //mvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+        MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get("/home")
+                .accept(MediaType.APPLICATION_JSON_VALUE)).andReturn();
+    }
+
+}
